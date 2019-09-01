6d950a4aedbb5df5b1d88a567dbbf9f82ce675c1
新城 <602006050@qq.com>
2019-09-01 20:31:52 +0800

    保存图片之前获取sku表数据

---
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/images.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+# encoding: utf-8
+"""
+@file: images.py
+@author: www.xcooo.cn
+@Mail: 602006050@qq.com
+"""
+from rest_framework import serializers
+from goods.models import SKUImage
+from goods.models import SKU
+
+class ImageSerializer(serializers.ModelSerializer):
+    """
+    图片序列化器
+    """
+    # 关联嵌套序列化字段
+    sku_id = serializers.IntegerField()
+    class Meta:
+        model = SKUImage
+        fields = '__all__'
+
+
+class ImageIdSerializer(serializers.ModelSerializer):
+    '''
+    图片id序列化器
+    '''
+    class Meta:
+        model = SKU
+        fields = ('id','name')
--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views/images.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views/images.py
@@ -5,3 +5,36 @@
 @author: www.xcooo.cn
 @Mail: 602006050@qq.com
 """
+from rest_framework.decorators import action
+from rest_framework.viewsets import ModelViewSet
+from goods.models import SKUImage, SKU
+from meiduo_admin.serializers.images import ImageSerializer, ImageIdSerializer
+from meiduo_admin.utils import PageNum
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+
+
+class ImagesViewset(ModelViewSet):
+    """
+    spu图片管理
+    """
+    # 指定模型类
+    queryset = SKUImage.objects.all()
+    # 指定序列化器
+    serializer_class = ImageSerializer
+
+    # 指定分页器
+    pagination_class = PageNum
+
+    # 指定权限
+    permission_classes = [IsAdminUser]
+
+
+    @action(methods=['GET'],detail=True)
+    def simple(self, request):
+        """
+        图片id获取
+        """
+        ids = SKU.objects.all()
+        ser = ImageIdSerializer(ids, many=True)
+        return Response(ser.data)
