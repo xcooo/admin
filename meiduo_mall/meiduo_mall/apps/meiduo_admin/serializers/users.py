bc2952f036c5f30a340bf4b1faf4d887f8045b34
新城 <602006050@qq.com>
2019-09-01 14:51:57 +0800

    改写create方法完成密码加密

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/users.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/users.py
@@ -11,6 +11,31 @@ from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
-        fields = ('id','username','mobile','email')
+        fields = ('id','username','mobile','email', 'password') # id默认是read_only
+
+        extra_kwargs = {
+            'password':{
+                'write_only':True,
+                'max_length':20,
+                'min_length':6,
+            },
+
+            'username': {
+                'max_length': 20,
+                'min_length': 3,
+            }
+        }
+
+    # 密码需要加密, 需要重写create方法(两种方法)
+    def create(self, validated_data):
+        # user = super().create(validated_data)
+        # # 密码加密的方法
+        # user.set_password(validated_data['password'])
+        # user.save()
+
+        # 第二种方法, 管理器方法
+        user = User.objects.create_user(**validated_data)
+        return user
--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views/users.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views/users.py
@@ -5,21 +5,21 @@
 @author: www.xcooo.cn
 @Mail: 602006050@qq.com
 """
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView
 
 from meiduo_admin.serializers.users import UserSerializer
 from meiduo_admin.utils import PageNum
 from users.models import User
 
 
-class UserView(ListAPIView):
+class UserView(ListCreateAPIView):
     """
     获取用户数据(单个和多个, 在同一个url路径请求下)
     """
     # 使用分页器
     pagination_class = PageNum
-    # 指定查询集
-    queryset = User.objects.all()
+    # 指定查询集  (重写后就不需要指定)
+    # queryset = User.objects.all()
     # 指定序列化器
     serializer_class = UserSerializer
 
