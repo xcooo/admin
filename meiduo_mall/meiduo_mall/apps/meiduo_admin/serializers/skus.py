c5fcda618ab69a8cc7e8d646b2c0945b6e69cdf3
新城 <602006050@qq.com>
2019-09-02 21:39:20 +0800

    获取规格信息实现(嵌套序列化规格选项)

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/skus.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/skus.py
@@ -6,7 +6,7 @@
 @Mail: 602006050@qq.com
 """
 from rest_framework import serializers
-from goods.models import SKU
+from goods.models import SKU, SPUSpecification, SpecificationOption
 from goods.models import GoodsCategory
 
 class SkuSerializer(serializers.ModelSerializer):
@@ -24,4 +24,26 @@ class CategorySerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = GoodsCategory
+        fields = '__all__'
+
+
+class SpecOptionSerializer(serializers.ModelSerializer):
+    '''
+    规格选项序列化器
+    '''
+    class Meta:
+        model = SpecificationOption
+        fields = '__all__'
+
+
+class SPUSpecSerializer(serializers.ModelSerializer):
+    """
+    规格序列化器
+    """
+    # 嵌套序列化返回规格选项 (如果指定 related_name, 就不能使用 模型类名小写_set)
+    options = SpecOptionSerializer(many=True)
+    # specificationoption_set = SpecOptionSerializer(many=True)
+
+    class Meta:
+        model = SPUSpecification
         fields = '__all__'
--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views/skus.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views/skus.py
@@ -8,8 +8,8 @@
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from goods.models import SKU
-from meiduo_admin.serializers.skus import SkuSerializer, CategorySerializer
+from goods.models import SKU, SPU
+from meiduo_admin.serializers.skus import SkuSerializer, CategorySerializer, SPUSpecSerializer
 from meiduo_admin.utils import PageNum
 from rest_framework.permissions import IsAdminUser
 from goods.models import GoodsCategory
@@ -32,6 +32,8 @@ class SkuView(ModelViewSet):
     # 指定权限类
     permission_classes = [IsAdminUser]
 
+    # 生成自动生成自定义方法的路由时, 需要给视图添加action装饰器
+    # detail=True 是传入了pk值, 如果没有传pk值, detail=False
     @action(methods=['get'], detail=False)
     def categories(self, request):
         """
@@ -39,4 +41,22 @@ class SkuView(ModelViewSet):
         """
         data = GoodsCategory.objects.filter(subs__id=None)
         ser = CategorySerializer(data, many=True)
+        return Response(ser.data)
+
+    def specs(self, request, pk):
+        '''
+        获取spu商品规格信息
+        :param request:
+        :param pk: spu表的 id值
+        :return:
+        '''
+        # 1.通过id值, 查询spu对象
+        spus = SPU.objects.get(pk=pk)
+
+        # 2.关联查询spu所关联的规格表 (如果指定 related_name, 就不能使用 模型类名小写_set)
+        data = spus.specs.all()
+
+        # 3.序列化返回多个规格信息 (通过规格表就可以嵌套序列化返回规格选项)
+        ser = SPUSpecSerializer(data, many=True)  # 规格也有多个
+
         return Response(ser.data)
