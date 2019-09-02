22918a672867fb8f09304c593132bf6ebc1b8dd9
新城 <602006050@qq.com>
2019-09-02 16:33:56 +0800

    详情页面静态化处理及异步任务运用

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/images.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/images.py
@@ -13,6 +13,7 @@ from rest_framework.response import Response
 
 from goods.models import SKUImage
 from goods.models import SKU
+from celery_tasks.static_file.tasks import get_detail_html
 
 class ImageSerializer(serializers.ModelSerializer):
     """
@@ -39,7 +40,34 @@ class ImageSerializer(serializers.ModelSerializer):
         # 6.保存图片表
         sku = validated_data['sku']  # 验证后的数据是一个对象
         img = SKUImage.objects.create(sku_id=sku.id, image=res['Remote file_id'])
+
+        # 异步生成详情页静态页面
+        get_detail_html.delay(img.sku.id)  # 此处不能传对象
+
         return img
+
+
+    def update(self, instance, validated_data):
+        # 3.建立fastdfs的客户端
+        client = Fdfs_client(settings.FASTDFS_PATH)
+
+        # 4.上传upload图片
+        # self.context['request']  # 获取request对象
+
+        image = self.context['request'].FILES.get('image')
+        res = client.upload_appender_by_buffer(image.read())  # 文件byte数据
+
+        # 5.判断图片是否上传成功
+        if res['Status'] != 'Upload successed.':
+            raise serializers.ValidationError({'error':'图片上传失败'})
+
+        # 6.更新图片表
+        instance.image = res['Remote file_id']
+        instance.save()
+
+        # 异步生成详情页静态页面
+        get_detail_html.delay(instance.sku.id)  # 此处不能传对象
+        return instance
 
 
 class ImageIdSerializer(serializers.ModelSerializer):
