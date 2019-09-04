069d4a918d1748c19665f6674159dd7500f50ed9
新城 <602006050@qq.com>
2019-09-04 20:54:22 +0800

    sku表更新逻辑实现

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/skus.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/skus.py
@@ -10,12 +10,26 @@ from goods.models import SKU, SPUSpecification, SpecificationOption, SKUSpecific
 from goods.models import GoodsCategory
 from django.db import transaction
 
+
+class SKUSpecificationSerializer(serializers.ModelSerializer):
+    '''
+    sku具体规格选项序列化器
+    '''
+    spec_id = serializers.IntegerField()
+    option_id = serializers.IntegerField()
+
+    class Meta:
+        model = SKUSpecification
+        fields = ('spec_id', 'option_id')
+
 class SkuSerializer(serializers.ModelSerializer):
     """
     sku序列化器
     """
     spu_id = serializers.IntegerField()
     category_id = serializers.IntegerField()
+    specs = SKUSpecificationSerializer(read_only=True, many=True)
+
     class Meta:
         model = SKU
         fields = '__all__'
@@ -44,6 +58,33 @@ class SkuSerializer(serializers.ModelSerializer):
                 # 提交
                 transaction.savepoint_commit(save_point)
                 return sku
+
+    def update(self, instance, validated_data):
+        # 获取规格信息
+        specs = self.context['request'].data.get('specs')
+        # 因为sku表中没有specs字段，所以在保存的时候需要删除validated_data中specs数据
+
+        with transaction.atomic():
+            # 开启事务
+            sid = transaction.savepoint()
+            try:
+                # 1、更新sku表
+                SKU.objects.filter(id=instance.id).update(**validated_data)
+
+                # 2、更新SKU具体规格表
+                for spec in specs:
+                    # SKUSpecification.objects.filter(sku=instance).update(**spec)
+                    SKUSpecification.objects.create(sku=instance, spec_id=spec['spec_id'], option_id=spec['option_id'])
+            except:
+                # 捕获异常，说明数据库操作失败，进行回滚
+                transaction.savepoint_rollback(sid)
+                return serializers.ValidationError('数据库错误')
+            else:
+                # 没有捕获异常，数据库操作成功，进行提交
+                transaction.savepoint_commit(sid)
+                # 执行异步任务生成新的静态页面
+
+                return instance
 
 
 class CategorySerializer(serializers.ModelSerializer):
