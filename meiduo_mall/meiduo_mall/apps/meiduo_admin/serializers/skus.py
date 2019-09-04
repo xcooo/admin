dbd66e5906cfd6a8ea5e1471418db73621945d4a
新城 <602006050@qq.com>
2019-09-04 18:29:17 +0800

    保存sku表数据,重新create

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/skus.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/skus.py
@@ -6,21 +6,49 @@
 @Mail: 602006050@qq.com
 """
 from rest_framework import serializers
-from goods.models import SKU, SPUSpecification, SpecificationOption
+from goods.models import SKU, SPUSpecification, SpecificationOption, SKUSpecification
 from goods.models import GoodsCategory
+from django.db import transaction
 
 class SkuSerializer(serializers.ModelSerializer):
     """
     sku序列化器
     """
+    spu_id = serializers.IntegerField()
+    category_id = serializers.IntegerField()
     class Meta:
         model = SKU
         fields = '__all__'
+        read_only_fields = ('spu','category') # 不参与反序列化过程
+
+    # @transaction.atomic()
+    def create(self, validated_data):
+        specs = self.context['request'].data.get('specs')
+        # 开启事物
+        with transaction.atomic():
+            # 设置保存点
+            save_point = transaction.savepoint()
+            try:
+                # 保存sku表
+                sku = SKU.objects.create(**validated_data)
+
+                # 保存sku具体规格表
+                for spec in specs:
+                    SKUSpecification.objects.create(spec_id = spec['spec_id'],
+                                                    option_id = spec['option_id'],sku=sku)
+            except:
+                # 回滚
+                transaction.savepoint_rollback(save_point)
+                raise serializers.ValidationError('保存失败')
+            else:
+                # 提交
+                transaction.savepoint_commit(save_point)
+                return sku
 
 
 class CategorySerializer(serializers.ModelSerializer):
     """
-    分类id序列化器
+    商品分类序列化器
     """
     class Meta:
         model = GoodsCategory
