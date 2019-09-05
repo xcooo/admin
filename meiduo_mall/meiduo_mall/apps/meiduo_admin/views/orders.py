8e738671c0d124b648ce138f3359367e778872d0
新城 <602006050@qq.com>
2019-09-05 12:00:17 +0800

    修改订单状态

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views/orders.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views/orders.py
@@ -5,6 +5,8 @@
 @author: www.xcooo.cn
 @Mail: 602006050@qq.com
 """
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.permissions import IsAdminUser
 
@@ -35,3 +37,31 @@ class OrderView(ReadOnlyModelViewSet):
             return OrderInfo.objects.all()
         else:
             return OrderInfo.objects.filter(order_id__contains=self.request.query_params.get('keyword'))
+
+    @action(methods=['put'], detail=True)
+    def status(self, request, pk):
+        """
+        修改订单状态
+        :param request:
+        :param pk: 订单id
+        :return: ok
+        """
+        # 1.通过pk值获取订单对象
+        try:
+            order = OrderInfo.objects.get(order_id=pk)
+        except:
+            return Response({'error':'订单编号错误'})
+
+        # 2.修改订单状态
+        # 获取订单状态
+        status = request.data.get('status')
+        if status is None:
+            return Response({'error': '缺少状态值'})
+        order.status = status
+        order.save()
+
+        # 3.返回修改信息
+        return Response({
+            'order_id':pk,
+            'status':status
+        })
